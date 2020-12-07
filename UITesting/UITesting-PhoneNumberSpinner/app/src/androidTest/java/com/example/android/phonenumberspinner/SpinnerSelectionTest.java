9db2c4c25153ca14c642aa6258e0af2260142682
Tung Le <thanhtung3496@gmail.com>
2020-12-07 05:25:20 -0500

    Finished UITesting-PhoneNumberSpinner

--- UITesting/UITesting-PhoneNumberSpinner/app/src/androidTest/java/com/example/android/phonenumberspinner/SpinnerSelectionTest.java
+++ UITesting/UITesting-PhoneNumberSpinner/app/src/androidTest/java/com/example/android/phonenumberspinner/SpinnerSelectionTest.java
@@ -35,11 +35,6 @@ import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 @RunWith(AndroidJUnit4.class)
 public class SpinnerSelectionTest {
     @Rule
@@ -48,30 +43,20 @@ public class SpinnerSelectionTest {
 
     @Test
     public void useAppContext() throws Exception {
-        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.example.android.phonenumberspinner",
                 appContext.getPackageName());
     }
 
-    /**
-     * Iterate through the spinner, selecting each item and
-     * checking to see if it matches the string in the array.
-     */
     @Test
     public void iterateSpinnerItems() {
         String[] myArray = mActivityRule.getActivity().getResources()
                 .getStringArray(R.array.labels_array);
-        // Iterate through the spinner array of items.
+
         int size = myArray.length;
         for (int i=0; i<size; i++) {
-            // Find the spinner and click on it.
             onView(withId(R.id.label_spinner)).perform(click());
-            // Find the spinner item and click on it.
             onData(is(myArray[i])).perform(click());
-            // Find the text view and check that the spinner item
-            // is part of the string.
             onView(withId(R.id.text_phonelabel))
                     .check(matches(withText(containsString(myArray[i]))));
         }
